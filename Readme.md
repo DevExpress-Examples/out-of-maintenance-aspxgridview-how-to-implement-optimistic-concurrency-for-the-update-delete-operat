c4ae43d378c480cbfc0c59f8a1dfd241634adaf1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ASPxGridView - How to implement optimistic concurrency for the update/delete operations
+
+
+<p>This approach is based on adding a field to the underlying data source that will contain information about the current row's version. All you need is to store this field's value and then assign it to e.OldValues["row_version_field_name"] in the ASPxGridView.RowUpdating/ASPxGridView.RowDeleting events. </p><p>Then the data source will automatically get old values for an edited/deleted record and compare it with actual data.</p><p><a href="http://www.asp.net/web-forms/tutorials/data-access/accessing-the-database-directly-from-an-aspnet-page/implementing-optimistic-concurrency-with-the-sqldatasource-vb"><u>Implementing Optimistic Concurrency with the SqlDataSource</u></a><u><br />
+</u><a href="https://www.devexpress.com/Support/Center/p/E3213">How to implement optimistic concurrency for the update operation</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private Const errorMessage As String = "This record is in use. Please refresh the page to obtain current data"
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		grid.DataSource = dsCategories
+		grid.DataBind()
+	End Sub
+	Protected ReadOnly Property RowHashes() As Dictionary(Of Object, String)
+		Get
+			If Session(SessionKey) Is Nothing Then
+				Session(SessionKey) = New Dictionary(Of Object, String)()
+			End If
+			Return CType(Session(SessionKey), Dictionary(Of Object, String))
+		End Get
+	End Property
+	Protected ReadOnly Property SessionKey() As String
+		Get
+			If (Not oldValuesStorage.Contains("PageSessionKey")) Then
+				oldValuesStorage("PageSessionKey") = Guid.NewGuid().ToString()
+			End If
+			Return oldValuesStorage("PageSessionKey").ToString()
+		End Get
+	End Property
+	Protected Sub ASPxGridView1_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+		e.OldValues("Version") = RowHashes(e.Keys(0))
+		e.NewValues("Version") = Integer.Parse(e.OldValues("Version").ToString()) + 1
+	End Sub
+	Protected Sub ASPxGridView1_RowDeleting(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataDeletingEventArgs)
+		e.Values("Version") = RowHashes(e.Keys(0)) + 1
+	End Sub
+	Protected Sub ASPxGridView1_RowInserting(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataInsertingEventArgs)
+		e.NewValues("Version") = 0
+	End Sub
+	Protected Sub ASPxGridView1_RowUpdated(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatedEventArgs)
+		If e.AffectedRecords = 0 Then
+			Throw New Exception(errorMessage)
+		End If
+	End Sub
+	Protected Sub ASPxGridView1_RowDeleted(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataDeletedEventArgs)
+		If e.AffectedRecords = 0 Then
+			Throw New Exception(errorMessage)
+		End If
+	End Sub
+	Protected Sub grid_CustomJSProperties(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewClientJSPropertiesEventArgs)
+		For i As Integer = 0 To grid.VisibleRowCount - 1
+			RowHashes(grid.GetRowValues(i, grid.KeyFieldName)) = grid.GetRowValues(i, "Version").ToString()
+		Next i
+	End Sub
+End Class
